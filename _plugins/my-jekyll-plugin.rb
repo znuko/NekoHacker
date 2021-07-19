9ed8ced3c097e86dd781e8122f01eb1ee034bdaa
znuko <nkymgenius@gmail.com>
2021-07-19 19:47:12 +0900

    Update my-jekyll-plugin.rb

--- _plugins/my-jekyll-plugin.rb
+++ _plugins/my-jekyll-plugin.rb
@@ -53,140 +53,195 @@ module Jekyll
       #   end
       # end
 
+tracks = site.data["tracks"]
+artists = site.data["artists"]
+projects = site.data["projects"]
 
-      tracks = site.data["tracks"]
-      artists = site.data["artists"]
-      projects = site.data["projects"]
+warnings = []
+errors = []
 
-      tracks.each do |track|
-        if track["artists"]
-          # puts track["artists"]
-          # track["artists"] = 
-          track["artists"].clean!
-          # puts track["artists"]
-        end
+# cleaning
+## nilなら[]で置き換え
+tracks.each do |t|
+  t["artists"] = t["artists"] || []
+#   t["projects"] = t["projects"] || []
+end
+
+
+#
+# データの欠損を補う
+#
+
+## track["title"]とtrack["short_title"]のどちらかが欠けていれば、一方の値で補う
+tracks.each do |track|
+  if track["title"].nil? || track["title"].empty?
+    track["title"] = track["short_title"]
+  elsif track["short_title"].nil? || track["title"].empty?
+    track["short_title"] = track["title"]
+  end
+end
+
+## artistのroleを配列にする
+tracks.each do |track|
+  track["artists"].each do |artist|
+    artist["roles"] = artist["roles"] || []
+    artist["roles"].push(artist["role"]).compact!
+    artist["roles"].each{|r| r&.downcase!}
+  end
+end
+
+## project["tracks"][key]のvalueがひとつも登録されていなければ、
+## project["tracks"][0]["title"]をproject["title"]にする
+projects.each do |project|
+  tracks_values = project["tracks"]&.map {|t| t&.values} &.join
+  if tracks_values.nil? || tracks_values.empty?
+    project["tracks"] = [{"title" => project["title"]}]
+  end
+end
+
+## projectのtrack numberを設定
+projects.each do |project|
+  num = 1
+  project["tracks"].each do |track|
+    if !track["num"].is_a?(Integer)
+      track["num"] = num
+    end
+    num += 1
+  end
+end
+
+
+
+#
+# idの設定とファイル間の関連付け
+#
+
+## track["id"]を設定
+tracks.each do |track|
+  track["id"] = URI.escape(track["title"].gsub(" ", "_"))
+  track["url"] = site.config["baseurl"].chomp("/") + "/#" + track["id"]
+end
+
+## artist["id"]を設定
+artists.each do |artist|
+  artist["id"] = URI.escape(artist["name"].gsub(" ", "_"))
+  artist["url"] = site.config["baseurl"].chomp("/") + "/artist/#" + URI.escape(artist["name"].gsub(" ", "_"))
+end
+
+## projectのidを設定
+projects.each do |project|
+  project["id"] = URI.escape(project["title"].gsub(" ", "_"))
+  project["url"] = site.config["baseurl"].chomp("/") + "/project/#" + project["id"]
+end
+
+
+## trackとartistの関連付け
+## artist idとtrack idをそれぞれ追加
+## もしartists.ymlにデータがなければ、artist idをnilにする
+artists.each {|a| a["vocal_ids"] = []}
+artists.each {|a| a["track_ids"] = []}
+tracks.each do |track|
+  track["artists"].each do |artist|
+    matching_artist = artists.select {|a| a["name"] == artist["name"]} [0]
+    if matching_artist.nil?
+      warnings.push("no data in artists.yml: #{artist["name"]} | #{artist["roles"]} | #{track["short_title"]}")
+      artist["id"] = nil
+    else
+      artist["id"] = matching_artist["id"]
+      if artist["roles"].include?("vocal")
+        matching_artist["vocal_ids"].push(track["id"]).uniq!
+      else
+        matching_artist["track_ids"].push(track["id"]).uniq!
       end
-      
-      ## track["title"]とtrack["short_title"]のどちらかが欠けていれば、一方の値で補う
-      tracks.each do |track|
-        if track["title"].nil? || track["title"].empty?
-          track["title"] = track["short_title"]
-        elsif track["short_title"].nil? || track["title"].empty?
-          track["short_title"] = track["title"]
-        end
-      end
+    end
+  end
+end
 
-      ## track["id"]を設定
-      tracks.each do |track|
-        track["id"] = URI.escape(track["title"].gsub(" ", "_"))
-      end
+## trackとprojectの関連付け
+## track idとproject idをそれぞれ追加
+## もしtracks.ymlにデータがなければ、idをnilにする
+tracks.each {|t| t["projects"] = t["projects"] || []}
+projects.each do |project|
+  project["tracks"].each do |track|
+    matching_track = tracks.select {|t| t["title"] == track["title"]} [0]
+    if matching_track.nil?
+      warnings.push("no data in tracks.yml: #{track["title"]}")
+      track["id"] = nil
+    else
+      track["id"] = matching_track["id"]
+      matching_track["projects"].push({
+        "id" => project["id"],
+        "track_number" => track["num"]
+        }).uniq!
+    end
+  end
+end
 
-      ## artist["id"]を設定
-      artists.each do |artist|
-        artist["id"] = URI.escape(artist["name"].gsub(" ", "_"))
-      end
+#
+# データの追加
+#
 
-      ## projectのidを設定
-      projects.each do |project|
-        project["id"] = URI.escape(project["title"].gsub(" ", "_"))
-      end
+## trackのartistに、artistsページへのURLを追加
+tracks.each do |track|
+  track["artists"].each do |artist|
+    next if artist["id"].nil?
+    matching = artists.select {|a| a["id"] == artist["id"]} [0]
+    artist["url"] = matching["url"]
+  end
+end
 
-      ## artist[key]のvalueがひとつも登録されていなければ、artistsをnilにする
-      tracks.each do |track|
-        # artist_values = track["artists"]&.map {|a| a&.values} &.join
-        # if artist_values.nil? || artist_values.empty?
-        #   track["artists"] = nil
-        # end
+## projectのtrackに、tracksページへのURLを追加
+projects.each do |project|
+  project["tracks"].each do |track|
+    next if track["id"].nil?
+    matching = tracks.select {|t| t["id"] == track["id"]} [0]
+    track["url"] = matching["url"]
+  end
+end
 
-        # track["artists"] = clean(track["artists"])
-        # track["artists"]&.clean!
-      end
+## trackにfirst release dateとsortのための番号を設定
+tracks.each do |track|
+  sorting_sets = []
+  track["projects"].each do |project|
+    matching_project = projects.select {|p| p["id"] == project["id"]} [0]
+    sort_num = matching_project["release"].to_s + "-" + format('%02d', project["track_number"]).to_s
+    sorting_sets.push({
+      "date" => matching_project["release"],
+      "num" => project["track_number"],
+      "sort" => sort_num
+    })
+  end
+  
+  sort_num = track["release"].to_s + "-" + format('%02d', 0).to_s
+  sorting_sets.push({
+    "date" => track["release"],
+    "num" => 0,
+    "sort" => sort_num
+  }).select! {|set| !set["date"].nil?}
 
-      ## track["artist"]がartists.ymlで登録されているかの真理値を追加
-      registered_artist_names = artists.map {|a| a["name"]}
-      complete_tracks = tracks.select {|t| t["artists"]}
-      complete_tracks.each do |track|
-        track["artists"].each do |artist|
-          if registered_artist_names.include?(artist["name"])
-            artist["is_registered"] = true
-          else
-            # artist["is_registered"] = false
-          end
-        end
-      end
+  sorting_set = sorting_sets.min_by {|d, n, s| s}
 
+  if sorting_set.nil?
+    warnings.push("no release setting: #{track["title"]}")
+  else
+    track["first_release"] = sorting_set["date"]
+    track["sort"] = sorting_set["sort"]
+  end
+end
 
-
-      ## artistページへのurlを設定
-      tracks.each do |track|
-        track["artists"]&.each do |artist|
-          if artist["is_registered"]
-            artist["url"] = site.config["baseurl"].chomp("/") + "/artist/#" + URI.escape(artist["name"].gsub(" ", "_"))
-          end
-        end
-      end
-
-      ## project["tracks"][key]のvalueがひとつも登録されていなければ、
-      ## project["tracks"][0]["title"]をproject["title"]にする
-      projects.each do |project|
-        tracks_values = project["tracks"]&.map {|t| t&.values} &.join
-        if tracks_values.nil? || tracks_values.empty?
-          project["tracks"] = [{"title" => project["title"]}]
-        end
-      end
-
-
-      ## trackにprojectからの情報を追加
-      tracks.each do |track|
-        ## track["projects"][key]のvalueがひとつも登録されていなければ、
-        ## track["projects"]を[]にする
-        values = track["projects"]&.map {|t| t&.values} &.join
-        if values.nil? || values.empty?
-          track["projects"] = []
-        end
-        ## projects.ymlからprojectを探して設定
-        projects.each do |project|
-          project_titles = project["tracks"]&.map {|t| t["title"]}
-          if project_titles&.include?(track["title"])
-            project_url = site.config["baseurl"].chomp("/") + "/project/#" + project["id"]
-            track["projects"].push({
-              "title" => project["title"],
-              "type" => project["type"],
-              "release" => project["release"],
-              "url" => project_url
-            })
-          end
-        end
-        ## 結局、projectがなければ[]からnilに戻す
-        if track["projects"] == []
-          track["projects"] = nil
-        end
-      end
-
-      ## project["tracks"]のtrackがtracks.ymlで登録されているかの真理値を追加
-      registered_track_titles = tracks.map {|t| t["title"]}
-      projects.each do |project|
-        project["tracks"]&.each do |track|
-          if registered_track_titles.include?(track["title"])
-            track["is_registered"] = true
-          else
-            track["is_registered"] = false
-          end
-        end
-      end
-
-      ## projects["tracks"]にtrackへのURLを追加
-      projects.each do |project|
-        project["tracks"].each do |track|
-          if track["is_registered"]
-            track["url"] = site.config["baseurl"].chomp("/") + "/#" + URI.escape(track["title"].gsub(" ", "_"))
-          end
-        end
-      end
+## tracksをreleas順でソート
+sortable_tracks = tracks.select {|t| t["sort"]} .sort_by! {|t| t["sort"]}
+unsortable_tracks = tracks.select {|t| !t["sort"]}
+site.data["tracks"] = sortable_tracks + unsortable_tracks
 
 
 
 
+
+####
+site.config["warnings"] = warnings
+site.config["errors"] = errors
+##################################################
 
     end # of def
 
